cccb8009763f487dbadc9a62fd41edf5cd65e960
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-09-18 14:14:05 -0500

    initial commit: creating microservice

---
+++ candresborja-munozd-nmontanez/src/main/java/micro/taller/candresborjamunozdnmontanez/CandresborjaMunozdNmontanezApplication.java
@@ -0,0 +1,13 @@
+package micro.taller.candresborjamunozdnmontanez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandresborjaMunozdNmontanezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandresborjaMunozdNmontanezApplication.class, args);
+	}
+
+}
---
+++ candresborja-munozd-nmontanez/src/test/java/micro/taller/candresborjamunozdnmontanez/CandresborjaMunozdNmontanezApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.taller.candresborjamunozdnmontanez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandresborjaMunozdNmontanezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
